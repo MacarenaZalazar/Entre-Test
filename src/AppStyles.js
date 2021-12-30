6804d0148d107cc293ca7ee5557798c08b76d060
Macarena Zalazar <82382979+MacarenaZalazar@users.noreply.github.com>
2021-12-30 17:17:46 -0300

    React app created

---
+++ src/AppStyles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const AppCont = styled.div`
+    background-color: #e5e5e5;
+    height: 100%;
+    padding: 50px;
+    display: grid;
+    grid-gap: 50px;
+    justify-content: space-around;
+    align-items:center;
+`
